bef9dab79f7c445ca939a3b900ad41d90f99a456
nelsW70 <nelsfoltz@gmail.com>
2018-06-01 11:59:26 -0400

    refactor to functional stateless components

--- src/App.js
+++ src/App.js
@@ -56,39 +56,44 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { onChange, value, children } = this.props;
-    return (
-      <form>
-        { children }
-        <input
-          type="text"
-          onChange={ onChange }
-          value={ value }
-        />
-      </form>
-    )
-  }
+const Search = ({ onChange, value, children }) => {
+  return (
+    <form>
+      { children }
+      <input
+        type="text"
+        onChange={ onChange }
+        value={ value }
+      />
+    </form>
+  )
 }
 
-class Table extends Component {
-  render() {
-    const { list, searchTerm, removeItem } = this.props;
-    return (
+const Table = ({ list, searchTerm, removeItem }) => {
+  return (
       <div>
         {
           list.filter( isSearched(searchTerm) ).map(item =>
               <div key={ item.objectID }>
                 <h3> <a href={ item.url }> { item.title }</a> by { item.author}</h3>
                 <p>{ item.num_comments } Comments | { item.points } Points</p>
-                <button type="button" onClick={ () => removeItem(item.objectID) }>Remove</button>
+                <Button
+                  type="button"
+                  onClick={ () => removeItem(item.objectID) }>
+                  Remove me
+                </Button>
               </div>
           )
         }
       </div>
     )
   }
-}
+
+const Button = ({ onClick, children }) =>
+  <button
+    onClick={ onClick } >
+    { children }
+  </button>
+
 
 export default App;
