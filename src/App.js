66db0018a9088a40731d6646c76ad70417d96933
nelsW70 <nelsfoltz@gmail.com>
2018-06-17 14:49:34 -0400

    styling fix on load more button

--- src/App.js
+++ src/App.js
@@ -115,19 +115,23 @@ class App extends Component {
           </Row>
         </Grid>
 
-        <Table
-          list={ list }
-          searchTerm={ searchTerm }
-          removeItem={ this.removeItem }
-        />
+        <Grid>
+          <Row>
+          <Table
+            list={ list }
+            searchTerm={ searchTerm }
+            removeItem={ this.removeItem }
+          />
 
-      <div className="text-center alert">
-        <Button
-          className="btn btn-success"
-          onClick={ () => this.fetchTopStories(searchTerm, page + 1) }>
-          Load more
-        </Button>
-      </div>
+        <div className="text-center alert">
+          <Button
+            className="btn btn-success"
+            onClick={ () => this.fetchTopStories(searchTerm, page + 1) }>
+            Load more
+          </Button>
+        </div>
+        </Row>
+      </Grid>
 
       </div>
     );
